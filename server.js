50046c21c9b386b4a7b39003e6fb0ebbd605676e
venkatesh <venkatesh.uideveloper@gmail.com>
2023-06-24 12:29:25 +0100

    indentation

--- server.js
+++ server.js
@@ -14,5 +14,4 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
